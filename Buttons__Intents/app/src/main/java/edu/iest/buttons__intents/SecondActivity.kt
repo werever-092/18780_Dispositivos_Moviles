1ef43621540507793881e39efef1a18d7aa641de
werever-092 <123513983+werever-092@users.noreply.github.com>
2023-02-01 19:16:43 -0600

    Changes

--- Buttons__Intents/app/src/main/java/edu/iest/buttons__intents/SecondActivity.kt
+++ Buttons__Intents/app/src/main/java/edu/iest/buttons__intents/SecondActivity.kt
@@ -2,10 +2,15 @@ package edu.iest.buttons__intents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val iRec = intent.getStringExtra("Score")
+        val tvScore = findViewById<TextView>(R.id.tvFscore)
+        tvScore.setText("Score", "Felicidades Aprobaste con $iRec" )
     }
 }
