bb803d473e1a7b26a0638fbcbbaba6fadebc3303
werever-092 <123513983+werever-092@users.noreply.github.com>
2023-02-18 11:55:19 -0600

    Comparison

---
+++ Comparison/app/src/main/java/edu/iest/comparison/MainActivity.kt
@@ -0,0 +1,18 @@
+package edu.iest.comparison
+
+import android.app.Activity
+import android.os.Bundle
+import edu.iest.comparison.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
