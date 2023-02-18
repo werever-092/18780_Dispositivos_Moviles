ce5529c703ffdc83c45002491bc040ce3ecf7544
werever-092 <123513983+werever-092@users.noreply.github.com>
2023-02-18 13:58:25 -0600

    Comparison Final

--- Comparison/app/src/main/java/edu/iest/comparison/MainActivity.kt
+++ Comparison/app/src/main/java/edu/iest/comparison/MainActivity.kt
@@ -1,12 +1,18 @@
 package edu.iest.comparison
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Toast
 import edu.iest.comparison.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
 
     private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +20,39 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val adapter = ArrayAdapter.createFromResource(this, R.array.myNums, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        binding.spFirstNum.adapter = adapter
+        binding.spSecondNum.adapter = adapter
+
+        binding.bnChange.setOnClickListener {
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("Atencion").setMessage("Quieres comparar los dos numeos?")
+                .setCancelable(false)
+                .setPositiveButton("OK", DialogInterface.OnClickListener {
+                        dialogInterface, i ->
+                    compNums()
+                })
+                .setNegativeButton("Cancelar", DialogInterface.OnClickListener{
+                        dialogInterface, i ->
+                    Toast.makeText(this, "Una lastima :,(", Toast.LENGTH_LONG).show()
+                })
+                .show()
+        }
+
+    }
+
+    private fun compNums () {
+        val num1 = binding.spFirstNum.selectedItem.toString().toInt()
+        val num2 = binding.spSecondNum.selectedItem.toString().toInt()
+
+        if (num1 == num2) {
+            Toast.makeText(this, "Los valores son iguales", Toast.LENGTH_LONG).show()
+        } else if (num1 > num2) {
+            Toast.makeText(this, "$num1 es mayor", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "$num2 es mayor", Toast.LENGTH_LONG).show()
+        }
     }
 }
