a4fcaad596a244696590589de1b41459f1740d6e
werever-092 <luis-cuv00@live.com>
2023-03-13 18:53:33 -0600

    Games Changes

--- Games/app/src/main/java/com/example/games/MainActivity.kt
+++ Games/app/src/main/java/com/example/games/MainActivity.kt
@@ -1,11 +1,105 @@
 package com.example.games
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvWelcome: TextView
+    private lateinit var etName: EditText
+    private lateinit var bnSave: Button
+    private lateinit var swToggle: Switch
+    private val NAME_KEY = "name"
+    private val SWITCH_KEY = "switch_status"
+    private val NAME_INSTANCE = "name_instance"
+    private var name: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("PREFERENCIAS", "onCreate")
         setContentView(R.layout.activity_main)
+
+        var actionBar: ActionBar?
+        actionBar = supportActionBar
+        var colorDrawable: ColorDrawable
+        colorDrawable = ColorDrawable(Color.parseColor("#FF018786"))
+        actionBar!!.setBackgroundDrawable(colorDrawable)
+
+        inicializarVistas()
+
+        Log.d("PREFERENCIAS", savedInstanceState?.getString(NAME_KEY).toString())
+//nombre = savedInstanceState?.getString(NOMBRE_KEY).toString()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d("PREFERENCIAS", "onSaveInstanceState")
+        outState.putString(NAME_KEY, name )
+        outState?.run {
+            putString(NAME_KEY, name)
+        }
+// call superclass to save any view hierarchy
+        super.onSaveInstanceState(outState)
+
+    }
+
+    override fun onResume() {
+        Log.d("PREFERENCIAS", "onResume")
+        if(TextUtils.isEmpty(name)){
+            val miSharedPreferences = getSharedPreferences("PERSISTENCIA", MODE_PRIVATE)
+            name= miSharedPreferences.getString(NAME_KEY, "").toString()
+
+        }
+
+        tvWelcome.text = name
+        super.onResume()
+    }
+
+    override fun onStart() {
+        Log.d("PREFERENCIAS", "onStart")
+        super.onStart()
+    }
+
+    override fun onPause() {
+        Log.d("PREFERENCIAS", "onPause")
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        Log.d("PREFERENCIAS", "onDestroy")
+        super.onDestroy()
+    }
+
+    private fun cambiarTextoBienvenida(nombre: String) {
+        if (!TextUtils.isEmpty(nombre)) {
+            tvWelcome.text = "Bienvenido " + nombre
+        }
+    }
+
+    private fun inicializarVistas() {
+        tvWelcome = findViewById(R.id.tvWelcome)
+        etName = findViewById(R.id.tilName)
+        bnSave = findViewById(R.id.bnSave)
+        swToggle = findViewById(R.id.swToggle)
+
+        bnSave.setOnClickListener {
+            name = etName.text.toString()
+            cambiarTextoBienvenida(name)
+            val miSharedPreferences = getSharedPreferences("PERSISTENCIA", MODE_PRIVATE)
+            val editor = miSharedPreferences.edit()
+            editor.putString(NAME_KEY, name)
+            editor.apply()
+        }
+
     }
 }
