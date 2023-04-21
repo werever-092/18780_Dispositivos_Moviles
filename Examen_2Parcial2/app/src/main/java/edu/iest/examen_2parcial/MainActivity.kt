182f114c95a1dc1efd0784a3c8288a82b9556e0a
werever-092 <123513983+werever-092@users.noreply.github.com>
2023-04-20 23:10:04 -0600

    Examen_2Parcial

---
+++ Examen_2Parcial2/app/src/main/java/edu/iest/examen_2parcial/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.iest.examen_2parcial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
